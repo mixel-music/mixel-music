58a9954854bd09f6db0fff83f4c10e690e83e9b8
bunubbv <me@bunubbv.com>
2024-02-06 00:17:16 +0900

    test: extract and covert to thumbnail from media

--- server/image.py
+++ server/image.py
@@ -1,4 +1,5 @@
 from utils import *
+from PIL import Image
 
 router = APIRouter()
 
--- server/stream.py
+++ server/stream.py
@@ -1,19 +1,31 @@
 from utils import *
 
+import io #test
+from PIL import Image # test
+
 router = APIRouter()
 
 @router.get("/stream/{path:path}")
-async def api_stream(path: str, range: str = Header(None)):
+async def api_stream(path: str, range: str = Header(None), img: int = None):
   path_file = get_abs_path('test', f'{path}')
-  
+
   if check_ext_valid(str(path_file)) == False:
     raise HTTPException(status_code=404)
 
   music_file = File(path_file)
-  music_mime = music_file.mime[0]
+  music_mime = music_file.mime[0] # 해당 부분 extract_metadata와 통합 필요
 
   file_size = path_file.stat().st_size
   file_chunk = int(file_size * 0.5) # 50% of file size
+
+  ## 이미지 처리 부분 image.py로 이동하기, 테스트 용도로 이곳에 작성.
+  if img == 1:
+    img_size = (500, 500)
+
+    test = extract_metadata(path_file)
+    test_img = Image.open(io.BytesIO(test.m_image()))
+    test_img.thumbnail(img_size)
+    test_img.save('test.jpeg') # 정상작동, DB에 파일 등록 이후 data/cache에 일괄 저장하는 방향으로
 
   if range:
     audio_range = range.replace("bytes=", "").split("-")
@@ -27,6 +39,8 @@ async def api_stream(path: str, range: str = Header(None)):
   Ensure audio_end does not exceed the file size
   """
   audio_end = min(audio_end, file_size - 1)
+
+  # 아래 부분 다시 작성할 필요 있음
 
   with open(path_file, "rb") as path_file:
     path_file.seek(audio_start)
