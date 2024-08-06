d3efbe939d733066196a898a91cd4b9997b06f83
Junghyuk Kwon <kwon@junghy.uk>
2024-08-07 01:21:02 +0900

    refactor

--- web/src/routes/artists/+page.svelte
+++ web/src/routes/artists/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { PageData } from './$types';
-
   import TableBody from '$lib/components/elements/table-body.svelte';
   import TableCell from '$lib/components/elements/table-data.svelte';
   import TableRow from '$lib/components/elements/table-row.svelte';
@@ -12,35 +11,15 @@
   <title>{ data.title } • mixel-music</title>
 </svelte:head>
 
-<!-- {#if data.artistListItem.length > 0}
-  <CardItemGroup title={ data.title }>
-
-    {#each data.artistListItem as artist (artist.artisthash)}
-      <CardItem>
-
-        <div>
-          <ContentHead head={ artist.artist } />
-        </div>
-
-      </CardItem>
-    {/each}
-
-  </CardItemGroup>
-{/if} -->
-
 <div class="artist-content">
-
   <TableBody>
-  
     {#each data.artistListItem as artist (artist.artisthash)}
       <TableRow>
-        <TableCell bold text={ artist.artist } />
+        <TableCell text={ artist.artist } />
       </TableRow>
     {/each}
-  
   </TableBody>
-  
-  </div>
+</div>
 
 <style>
   .artist-content {
